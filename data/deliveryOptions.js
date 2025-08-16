5e52a25c71c276719e19a944cfdf7f61a0ae9ae0
Gaurav <gauravmor0001@gmail.com>
2025-08-16 14:14:54 +0530

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,3 +12,13 @@
     deliveryDays:1,
     priceCents:999
 }]
+
+export function getDeliveryOption(deliveryOptionId){
+    let deliveryOption;
+      deliveryOptions.forEach((option)=>{
+        if(option.id===deliveryOptionId){
+          deliveryOption=option;
+        }
+      });
+      return deliveryOption || deliveryOption[0];
+}
